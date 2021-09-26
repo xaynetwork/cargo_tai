fd8dd6833ea39c2b849b6af3c1626b2cb142f757
Robert Debug <robertt.debug@gmail.com>
2021-09-26 17:07:48 +0200

    static

--- tai-lib/src/ios/platform/mod.rs
+++ tai-lib/src/ios/platform/mod.rs
@@ -15,8 +15,8 @@ pub mod simulator;
 
 pub const APP_ID: &str = "cargo-tai";
 
-fn tasks_for_build_cmd() -> [Task; 7] {
-    [
+fn tasks_for_build_cmd() -> &'static [Task] {
+    &[
         Task::GetProjectMetadata(GetProjectMetadata),
         Task::BuildBuiltUnits(BuildBuiltUnits),
         Task::ReadSigningSettings(ReadSigningSettings), //we need the team id later + bundle id
